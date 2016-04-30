d7437c59d33f0d85f4d03df7dc19b3fcb79547b0
Milo Trujillo <milo.trujillo@sonomaacademy.org>
2016-04-30 01:48:38 -0400

    Initial push of website

--- puddle/client.rb
+++ puddle/client.rb
@@ -1,5 +1,38 @@
 require_relative "configuration"
+require_relative "log"
+
+=begin
+	This file is responsible for generating the UI every human
+	user will interact with. It calls out to other modules
+	to get work done.
+=end
+
+get '/client' do
+	redirect to("/client/")
+end
 
 get "/client/" do
-	return "Hello world!"
+	uptime = Log.uptime
+	erb :index, :locals => {:uptime => uptime}
+end
+
+get '/client/about' do
+	erb :about
+end
+
+get '/client/shared' do
+	erb :shared, :locals => {:sharedDir => Configuration::DataDir}
+end
+
+get '/client/downloads' do
+	erb :downloads
+end
+
+get '/client/cache' do
+	erb :cache
+end
+
+get '/client/log' do
+	data = Log.read
+	erb :log, :locals => {:log => data}
 end
