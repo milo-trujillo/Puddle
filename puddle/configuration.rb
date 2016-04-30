d555b160aa6e0664d42b4ac60ab733d8e3ce5c6c
Milo Trujillo <milo.trujillo@sonomaacademy.org>
2016-04-30 01:48:03 -0400

    Added public folder and log file

--- puddle/configuration.rb
+++ puddle/configuration.rb
@@ -1,9 +1,13 @@
 module Configuration
 	Base = File.dirname(__FILE__)
 	PrivateFolder = Base + "/private"
+	PublicFolder = Base + "/public"
 
 	# For network configuration
 	Port = 7924 # Swag
+
+	# For the logging system
+	LogFile = PrivateFolder + "/log.txt"
 	
 	# This is for the storage module
 	DataDir = PrivateFolder + "/data"
