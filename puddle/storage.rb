bfd160688eb4b7f2a8c6af1545bb4b61200caa0b
Fox <foxboy94@gmail.com>
2016-04-29 20:48:29 -0400

    Added a single function, not sure if it works atm

--- puddle/storage.rb
+++ puddle/storage.rb
@@ -2,8 +2,29 @@ require_relative "configuration"
 
 =begin
 	This module does things.
+	Cool things.
 =end
 
 module Storage
+
+	# returns an array of files with the specified tag if the tag is valid
+	def self.findfiles(tag)
+		# tags should be alphanumeric, and should not be '.' or '..'
+		# if these conditions are met, we iterate over the files that contain the tag
+		if(tag =~ \A[\w]+\z)
+			# create a new array to return
+			arr = Array.new
+			Dir.glob(Configuation::DataDir + '*' + tag + '*') do |myfile|
+				# this should create an array of all the files that contain the tag requested
+				str = myfile
+				arr.push str 
+			end
+			return arr
+		end
+		else # if the tag conditions aren't met
+			return nil
+		end
+	end
+
 
 end
