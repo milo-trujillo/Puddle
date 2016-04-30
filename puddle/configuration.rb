c0a8d7a8f5b87dbf70a9919c08e781fae3629d32
Milo Trujillo <milo.trujillo@sonomaacademy.org>
2016-04-29 20:15:32 -0400

    Added port number

--- puddle/configuration.rb
+++ puddle/configuration.rb
@@ -1,6 +1,9 @@
 module Configuration
 	Base = File.dirname(__FILE__)
 	PrivateFolder = Base + "/private"
+
+	# For network configuration
+	Port = 7924 # Swag
 	
 	# This is for the storage module
 	DataDir = PrivateFolder + "/data"
