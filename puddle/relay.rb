8a68ed16f2e640607a284ed8537f2e66cf8dba0a
Milo Trujillo <milo.trujillo@sonomaacademy.org>
2016-04-30 01:48:20 -0400

    Initializes logging system

--- puddle/relay.rb
+++ puddle/relay.rb
@@ -1,13 +1,17 @@
 #!/usr/bin/env ruby
 
 require "sinatra"
+require "tilt/erb"
 
 require_relative "configuration"
 require_relative "storage"
 require_relative "client"
+require_relative "log"
 
 set :bind => "0.0.0.0"
 set :port => Configuration::Port
+
+Log.init
 
 get '/' do
 	redirect to('/client/')
