0f0baa14fb1da8a4babdec2ee63da20ec026c20c
Milo Trujillo <milo.trujillo@sonomaacademy.org>
2016-05-01 16:31:28 -0400

    Connected to signalling code, added dummy filenames for templates

--- puddle/client.rb
+++ puddle/client.rb
@@ -1,5 +1,6 @@
 require_relative "configuration"
 require_relative "log"
+require_relative "signal"
 
 =begin
 	This file is responsible for generating the UI every human
@@ -7,13 +8,22 @@ require_relative "log"
 	to get work done.
 =end
 
+before '/client/*' do
+	# This is where we'll check to make sure you're connected from
+	# localhost, and redirect you to a forbidden page otherwise
+	if( request.ip != "127.0.0.1" )
+		halt 403
+	end
+end
+
 get '/client' do
 	redirect to("/client/")
 end
 
 get "/client/" do
 	uptime = Log.uptime
-	erb :index, :locals => {:uptime => uptime}
+	requests = Signal.activeRequests
+	erb :index, :locals => {:uptime => uptime, :requests => requests}
 end
 
 get '/client/about' do
@@ -21,18 +31,29 @@ get '/client/about' do
 end
 
 get '/client/shared' do
-	erb :shared, :locals => {:sharedDir => Configuration::DataDir}
+	files = ["illuminati.pdf"]
+	erb :shared, :locals => {:sharedDir => Configuration::DataDir, :files => files}
 end
 
 get '/client/downloads' do
-	erb :downloads
+	files = ["illuminati-song.mp3", "illuminati-proof.jpg"]
+	erb :downloads, :locals => {:files => files}
 end
 
 get '/client/cache' do
-	erb :cache
+	files = ["mcdonalds-menu.pdf", "SICP.pdf", "sheeple.png"]
+	erb :cache, :locals => {:files => files}
 end
 
 get '/client/log' do
-	data = Log.read
+	data = Log.read_backwards
 	erb :log, :locals => {:log => data}
+end
+
+post '/client/addRequest' do
+	request = params['request']
+	if( request != nil and request.length > 0 )
+		Signal.addRequest(request)
+	end
+	redirect to('/client/')
 end
